3e3eb3261aa79a11ccb4dce3d858ef42c285e3d3
Elnur Əlyarov <ealyarov@example.com>
2026-04-10 14:53:44 +0400

    Fix middleware, add all pages

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -92,4 +92,4 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   );
 }
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);   
