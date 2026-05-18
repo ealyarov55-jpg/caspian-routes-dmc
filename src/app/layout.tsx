ff8a81fa4560b7a2a2313a4af52671f210e44988
Elnur Əlyarov <ealyarov@example.com>
2026-05-18 23:14:16 +0400

    fix: remove travelpayouts from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import Script from "next/script";
-
 export default function RootLayout({
   children,
 }: {
@@ -7,23 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body>
-        {children}
-        <Script
-          id="travelpayouts-drive"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                var script = document.createElement("script");
-                script.async = 1;
-                script.src = 'https://tpembars.com/NTI0ODE4.js?t=524818';
-                document.head.appendChild(script);
-              })();
-            `,
-          }}
-        />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
